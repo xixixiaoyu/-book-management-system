8b68576a69235664b0a9243225db41dfb5e651bd
yunumu <1416900346@qq.com>
2024-10-19 21:37:26 +0800

    feat(user): 添加用户注册功能并进行数据验证
    - 在 user 模块中创建 RegisterUserDto 类，用于用户注册的数据传输
    - 在 user 控制器中添加 register 方法，处理用户注册请求
    - 在 main.ts 中全局注册 ValidationPipe，用于数据验证和转换

--- book-management-system-backend/src/main.ts
+++ book-management-system-backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 全局管道，用于验证 DTO 数据，transform: true 用于将请求参数转换为 DTO 对象
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
