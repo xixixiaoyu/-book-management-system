053f6745f5611e890d82c7ec7d0a620479862be5
yunumu <1416900346@qq.com>
2024-10-20 15:23:19 +0800

    build(frontend): 添加 antd 和 react-router-dom 依赖
    - 在 package.json 中添加 antd 依赖，版本为 ^5.21.4
    - 在 package.json 中添加 react-router-dom 依赖，版本为 ^6.27.0

--- book-management-system-frontend/src/main.tsx
+++ book-management-system-frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { BookManage } from './pages/BookManage'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = [
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/register',
+		element: <Register />,
+	},
+	{
+		path: '/',
+		element: <BookManage />,
+	},
+]
+
+const router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<RouterProvider router={router} />)
---
+++ book-management-system-frontend/src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+
+export function Login() {
+	return (
+		<div>
+			login
+			<Button type="primary">按钮</Button>
+		</div>
+	)
+}
